6f52243f552446a83420c01605935806482c06db
Islam Sayed <islam.sayed8@gmail.com>
2019-08-10 14:38:56 +0200

    inputs, handleClick func

---
+++ src/components/TodoInput.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+
+class TodoInput extends Component {
+    state = {  }
+    handleClick = (e) => {
+        e.preventDefault()
+        const inputItemValue = document.getElementById('todoInput').value;
+        const importantCheckValue = document.getElementById('important').checked;
+        const urgentCheckValue = document.getElementById('urgent').checked;
+        return this.props.addTodoItem(inputItemValue, importantCheckValue, urgentCheckValue)
+    } 
+    render() { 
+        return ( 
+            <div>   
+                <input type="text" id="todoInput" placeholder="Add todo" required />
+                <button id="add-button" onClick={this.handleClick}>Add</button>
+                <input type="checkbox" id="important" name="priority" />
+                <label htmlFor="important" >important</label>
+                <input type="checkbox" id="urgent" name="priority" />
+                <label htmlFor="urgent" >urgent</label>
+            </div>
+         );
+    }
+}
+ 
+export default TodoInput;
