eedae77a5e95aab9c0f398ab44e6c7a936593873
Islam Sayed <islam.sayed8@gmail.com>
2019-08-10 14:38:05 +0200

    component init

---
+++ src/components/TodosList.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import TodoItem from './TodoItem';
+
+const TodoList = () => {
+    return ( 
+        <div>
+            <ul>
+                <li>
+                    <TodoItem />
+                </li>
+                <li>
+                    <TodoItem />
+                </li>
+                <li>
+                    <TodoItem />
+                </li>
+                <li>
+                    <TodoItem />
+                </li>
+            </ul>
+        </div>
+     );
+}
+ 
+export default TodoList;
