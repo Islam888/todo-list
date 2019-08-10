9a6e6137f027a83453cbc3c555323647c94fe9c1
Islam Sayed <islam.sayed8@gmail.com>
2019-08-10 14:37:34 +0200

    todoItems state, showAddedTodoItem func

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import TodoInput from './components/TodoInput'
+import TodoList from './components/TodosList'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = { 
+    //List of todoItems objects
+    todoItems: [],
+  }
+
+  //function invoked after click of Add button to add and show a new todo
+  showAddedTodoItem = (todoItem, isImportant, isUrgent) => {
+    this.setState(prevState => ({
+      todoItems: prevState.todoItems.concat({
+        todoItem,
+          isImportant,
+          isUrgent,
+          isDone: false,
+      })
+    }))
+  } 
+  render() {
+    return (
+      <div className="App">
+        <TodoInput addTodoItem={this.showAddedTodoItem} />
+        <TodoList TodoItems={this.state.todoItems} />
+      </div>
+    );
+  }
 }
 
 export default App;
