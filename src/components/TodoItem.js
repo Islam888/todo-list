4873f3fdfe773b710fde370a3d78e294524737e2
Islam Sayed <islam.sayed8@gmail.com>
2019-08-11 09:55:50 +0200

    Add handlers for the three buttons

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -2,16 +2,24 @@ import React, { Component } from 'react';
 
 class TodoItem extends Component {
     state = {  }
+
+    handleRemoveBtnClick = () => this.props.removeTodoItem(this.props.id)
+
+    handleEditBtnClick = () => this.props.editTodoItem(this.props.id)
+
+    handleDoneBtnClick = () => this.props.markeTodoItemDone(this.props.id)
+
     render() { 
         return ( 
             <div>
                 <div>
-                    <button>edit</button>
-                    <button>remove</button>
-                    <button>done</button>
+                    <button onClick={this.handleEditBtnClick}>edit</button>
+                    <button onClick={this.handleRemoveBtnClick}>remove</button>
+                    <button onClick={this.handleDoneBtnClick}>done</button>
                 </div>
                 <div>
-                    <p>{this.props.item}</p>
+                
+                <p>{this.props.item}</p>
                 </div>
             </div>
          );
