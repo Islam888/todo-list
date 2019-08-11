629ba8b2af5c7ef889681dfc84135f1136a1ebf6
Islam Sayed <islam.sayed8@gmail.com>
2019-08-11 09:57:53 +0200

    add condition for not rendering empty text

--- src/components/TodoInput.js
+++ src/components/TodoInput.js
@@ -5,25 +5,35 @@ class TodoInput extends Component {
     handleClick = (e) => {
         e.preventDefault()
         const inputItemValue = document.getElementById('todoInput').value;
-        const importantCheckValue = document.getElementById('important').checked;
-        const urgentCheckValue = document.getElementById('urgent').checked;
-        const date = new Date().toJSON().slice(0,10).replace(/-/g,'/');
-        return this.props.addTodoItem(inputItemValue, importantCheckValue, urgentCheckValue, date)
-    } 
-
-    handleKeyDown = (e) => {
-        if (e.keyCode === 13) {
-            const inputItemValue = e.target.value;
+        if (!inputItemValue) {
+            return;
+        } else {
+            document.getElementById('todoInput').value = "" //empty the text field
             const importantCheckValue = document.getElementById('important').checked;
             const urgentCheckValue = document.getElementById('urgent').checked;
             const date = new Date().toJSON().slice(0,10).replace(/-/g,'/');
             return this.props.addTodoItem(inputItemValue, importantCheckValue, urgentCheckValue, date)
         }
+    } 
+
+    handleKeyDown = (e) => {
+        const inputItemValue = e.target.value;
+        if (e.keyCode === 13 && inputItemValue) {
+            e.target.value = "" //empty the text field
+            const importantCheckValue = document.getElementById('important').checked;
+            const urgentCheckValue = document.getElementById('urgent').checked;
+            const date = new Date().toJSON().slice(0,10).replace(/-/g,'/');
+            return this.props.addTodoItem(inputItemValue, importantCheckValue, urgentCheckValue, date)
+        } else {
+            return;
+        }
     }
+
+
     render() { 
         return ( 
             <div>   
-                <input type="text" id="todoInput" onKeyDown={this.handleKeyDown} placeholder="Add todo" required />
+                <input type="text" id="todoInput" onKeyDown={this.handleKeyDown} placeholder="Add todo" />
                 <button id="add-button" onClick={this.handleClick}>Add</button>
                 <input type="checkbox" id="important" name="priority" />
                 <label htmlFor="important" >important</label>
