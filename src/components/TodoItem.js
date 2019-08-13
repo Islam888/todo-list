9fec635d3afc5adfef64c4c59e7958b5d790a8f3
Islam Sayed <islam.sayed8@gmail.com>
2019-08-13 02:13:45 +0200

    change btn to checkbox

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,23 +1,41 @@
 import React from "react";
+import edit from "../img/edit.svg" 
+import remove from "../img/remove.svg" 
 
 const TodoItem = props => {
-  const { id, item, removeTodoItem, editTodoItem, markTodoItemDone } = props;
+  const { itemId, item, removeTodoItem, editTodoItem, markTodoItemDone } = props;
 
-  const handleRemoveBtnClick = () => removeTodoItem(id);
+  const handleRemoveBtnClick = () => removeTodoItem(itemId);
 
-  const handleEditBtnClick = () => editTodoItem(id);
+  const handleEditBtnClick = () => editTodoItem(itemId);
 
-  const handleDoneBtnClick = () => markTodoItemDone(id);
+  const handleDoneCheckboxClick = (e) => {
+    return markTodoItemDone(e.target.checked, itemId)
+  };
+  
 
   return (
     <div>
       <div>
-        <button onClick={handleEditBtnClick}>edit</button>
-        <button onClick={handleRemoveBtnClick}>remove</button>
-        <button onClick={handleDoneBtnClick}>done</button>
+        
+        
+
       </div>
-      <div>
-        <p>{item}</p>
+      <div className="todo-item">
+      <label className="control control-checkbox">
+          <input
+            type="checkbox"
+            id="done"
+            name="priority"
+            onChange={handleDoneCheckboxClick}
+          />
+          <div className="control_indicator rounded-checkbox" />
+        </label>
+        <p className="task-text" id={`task-text-${itemId}`}>{item}</p>
+        
+        <button className="remove-button" onClick={handleRemoveBtnClick}><img src={remove}  alt="remove" /></button>
+        <button className="edit-button" onClick={handleEditBtnClick}><img src={edit} alt="edit" /></button>
+        
       </div>
     </div>
   );
